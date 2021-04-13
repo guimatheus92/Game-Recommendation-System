17b4bb15bf73b76d8833363a606c33b05318e667
Guilherme Matheus <guilherme_matheus12@hotmail.com>
2021-04-12 22:44:18 -0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -19,7 +19,7 @@ def create_app():
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
 
-    from .models import User
+    from models import User
 
     @login_manager.user_loader    
     def load_user(user_id):
@@ -27,19 +27,19 @@ def create_app():
         return User.query.get(int(user_id))
 
     # blueprint for auth routes in our app
-    from .auth import auth as auth_blueprint
+    from auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint)
 
     # blueprint for non-auth parts of app
-    from .main import main as main_blueprint
+    from main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
     # blueprint for non-auth parts of app
-    from .games import games as main_blueprint
+    from games import games as main_blueprint
     app.register_blueprint(main_blueprint)
 
     # blueprint for non-auth parts of app
-    from .ml_utils import ml_utils as main_blueprint
+    from ml_utils import ml_utils as main_blueprint
     app.register_blueprint(main_blueprint)
 
     return app
