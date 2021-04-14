e33d70c31e7a882596e95a7b5e634c296dfc0fd2
Guilherme Matheus <guilherme_matheus12@hotmail.com>
2021-04-14 18:37:06 -0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -32,7 +32,9 @@ def create_app():
     login_manager.init_app(app)
 
     from models import User    
-
+    
+    db.create_all()
+    
     @login_manager.user_loader    
     def load_user(user_id):
         # since the user_id is just the primary key of our user table, use it in the query for the user
