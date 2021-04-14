5ab43685d99d60ac005a6d31f31c0b0b74cfb221
Guilherme Matheus <guilherme_matheus12@hotmail.com>
2021-04-14 17:25:18 -0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -14,7 +14,7 @@ def create_app():
     app = Flask(__name__)
 
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False    
-    app.config['SECRET_KEY'] = '9OLWxND4o83j4K4iuopO'    
+    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
     
     uri = os.environ.get("DATABASE_URL")  # or other relevant config var
     
