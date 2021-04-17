07e776a329f295125cbca84cb332766f21c591d4
Guilherme Matheus <guilherme_matheus12@hotmail.com>
2021-04-17 15:13:06 -0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -23,12 +23,12 @@ def create_app():
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
 
-    from models import *
+    from models import User
 
     @login_manager.user_loader    
     def load_user(user_id):
         # since the user_id is just the primary key of our user table, use it in the query for the user
-        return models.User.query.get(int(user_id))
+        return User.query.get(int(user_id))
 
     # blueprint for auth routes in our app
     from auth import auth as auth_blueprint
