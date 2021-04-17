c6328420a065963c07307bf3c6c2c4b1ba501463
Guilherme Matheus <guilherme_matheus12@hotmail.com>
2021-04-17 18:39:11 -0300

    Update models.py

--- models.py
+++ models.py
@@ -39,7 +39,7 @@ def check_gamesplayed():
     params = (str(userid))
     DATABASE_URL = os.environ['DATABASE_URL']
     conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-    df_checkgamesplayed = pd.read_sql_query('SELECT * FROM "USERGAMESPLAYED" WHERE "ID_USER" = ?', conn, params=(params))   
+    df_checkgamesplayed = pd.read_sql_query('SELECT * FROM "USERGAMESPLAYED" WHERE "ID_USER" = %s', conn, params=params)   
     conn.close()
     return df_checkgamesplayed
 
