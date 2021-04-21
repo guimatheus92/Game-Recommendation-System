5372b6e248470cd778a44c1049f3f13236a14101
Guilherme Matheus <guilherme_matheus12@hotmail.com>
2021-04-21 17:47:27 -0300

    Update auth.py

--- auth.py
+++ auth.py
@@ -3,7 +3,7 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_user, logout_user, login_required
-from models import User, check_gamesplayed
+from models import User
 from __init__ import db
 from games import save_ml_models
 import datetime
