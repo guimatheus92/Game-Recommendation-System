f5dc910124d2a2de2f3147f27819dfbe04b2ab8f
Guilherme Matheus <guilherme_matheus12@hotmail.com>
2021-04-22 23:45:02 -0300

    Update games.py

--- games.py
+++ games.py
@@ -37,20 +37,8 @@ def save_ml_models():
 	
 	title_train = df_gamesplayed[mask_train]['IMPORTANT_FEATURES']
 	title_val = df_gamesplayed[mask_val]['IMPORTANT_FEATURES']
-	
-	# LightGBM after optimization	
-	params = [0.059718899146636396, 2, 3, 0.5850311564924788, 0.8012679059209196, 688, 1, 3]
-	lr = params[0]
-	max_depth = params[1]
-	min_child_samples = params[2]
-	subsample = params[3]
-	colsample_bytree = params[4]
-	n_estimators = params[5]
-	
-	min_df = params[6]
-	ngram_range = (1, params[7])
-	
-	title_vec = TfidfVectorizer(min_df=min_df, ngram_range=ngram_range)
+		
+	title_vec = TfidfVectorizer(min_df=4, ngram_range=(1,3))
 	title_bow_train = title_vec.fit_transform(title_train)
 	title_bow_val = title_vec.transform(title_val)
 	
@@ -59,7 +47,7 @@ def save_ml_models():
 	Xtrain_wtitle = hstack([Xtrain, title_bow_train])
 	Xval_wtitle = hstack([Xval, title_bow_val])
 	
-	mdl_lgbm = LGBMClassifier(learning_rate=lr, num_leaves=2 ** max_depth, max_depth=max_depth, min_child_samples=min_child_samples, subsample=subsample, colsample_bytree=colsample_bytree, bagging_freq=1,n_estimators=n_estimators, random_state=0, class_weight="balanced", n_jobs=6)
+	mdl_lgbm = LGBMClassifier(random_state=0, class_weight="balanced", n_jobs=6)
 	mdl_lgbm.fit(Xtrain_wtitle, ytrain)
 	
 	try:
